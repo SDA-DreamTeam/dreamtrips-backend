80de924c90cba5a9fab0a1030ba551064c802057
Evgeniya Rubanova <e.rubanova@gmail.com>
2021-08-08 11:17:29 +0300

    adding Hotel, Airport models

---
+++ src/main/java/com/github/dreamteam/model/Airport.java
@@ -0,0 +1,39 @@
+package com.github.dreamteam.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "airport")
+public class Airport {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private String id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="CITY_ID", nullable=false, updatable=false)
+    private City city;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/com/github/dreamteam/model/Hotel.java
@@ -0,0 +1,48 @@
+package com.github.dreamteam.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private String id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="CITY_ID", nullable=false, updatable=false)
+    private City city;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
