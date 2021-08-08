0916a50d883c6e59367bee488986d03efc5751e2
Evgeniya Rubanova <e.rubanova@gmail.com>
2021-08-08 13:32:50 +0300

    adding City, Country, Hotel repositories

---
+++ src/main/java/com/github/dreamteam/repository/HotelRepository.java
@@ -0,0 +1,8 @@
+package com.github.dreamteam.repository;
+
+import com.github.dreamteam.model.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, String> {
+
+}
