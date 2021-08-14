97bde7070718c2438cd653c2e57b2d0ce6f64066
Evgeniya Rubanova <e.rubanova@gmail.com>
2021-08-14 10:14:00 +0300

    fix Airport repository

--- src/main/java/com/github/dreamteam/repository/AirportRepository.java
+++ src/main/java/com/github/dreamteam/repository/AirportRepository.java
@@ -3,5 +3,5 @@ package com.github.dreamteam.repository;
 import com.github.dreamteam.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AirportRepository extends JpaRepository<Airport, String> {
+public interface AirportRepository extends JpaRepository<Airport, Long> {
 }
